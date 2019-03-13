52dd830c0a4c205f011cc6e3e09a9c485ff61ee0
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-13 20:46:38 +0330

    Add display_credit messages

--- constants/messages.py
+++ constants/messages.py
@@ -1,8 +1,10 @@
 class BotMessages:
-    start_message = "سلام. به بات نیکو خوش آمدید"
+    start_message = "سلام. به بات نیکو خوش آمدید."
     get_code_message = "این کد مختص شماست!\n" \
                        "با استفاده از آن می‌توانید اقدام به دعوت نزدیکان برای اضافه شدن به " \
                        "لیستتان کنید."
     enter_code_message = "لطفا کد مربوطه را وارد کنید:\n"
     enter_code_confirmed = "شما با موفقیت اضافه شدید.\n"
     enter_code_failed = "مشکلی در اضافه‌شدن پیش‌آمده است.\n"
+    display_credit_message = "شما به مقدار زیر امتیاز دارید:\n"
+    credit_info_message = "هر امتیاز برابر با ۱۰۰۰ تومان اعتبار است.\n"
