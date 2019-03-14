08eebf3eb7b8f290505dbd453e51611e7b44b544
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-14 13:06:51 +0330

    Now we can place debt and notify the friends about the debt

--- handlers/request_debt_handler.py
+++ handlers/request_debt_handler.py
@@ -5,11 +5,16 @@
     debt request on the system and notifies all of the friends for pay
 """
 from telegram.ext import ConversationHandler, RegexHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
 from constants.button_messages import ButtonMessages as btm
 from constants.messages import BotMessages as bm
+import database.db_handler as db
 
 # Generating the states of this handler
 REQUEST_AMOUNT = range(1)
+
+# Notify Debt keyboard
+notify_debt_keyboard = [[btm.notify_debt]]
 
 
 # This method generates the whole stuffs about request_debt_handler
@@ -28,8 +33,8 @@ def request_debt_handler_entry(bot, update):
     user = update.message.from_user
     left_credit_amount = fetch_left_credit(user)
     update.message.reply_text(bm.request_debt_amount_message +
-                              "\n" +
                               str(left_credit_amount) +
+                              "\n" +
                               bm.request_debt_message)
 
     return REQUEST_AMOUNT
@@ -44,8 +49,13 @@ def get_request_amount(bot, update):
         update.message.reply_text(bm.request_debt_failed)
         return ConversationHandler.END
     # User has the required credit, so we place the debt request
-    # TODO: Place the debt request
-    update.message.reply_text(bm.request_debt_succeed)
+    debt_result = place_debt(user.id, requested_amount)
+    notify_friends(bot, user.id, debt_result)
+    if debt_result:
+        update.message.reply_text(bm.request_debt_succeed)
+    else:
+        update.message.reply_text(bm.unknown_error)
+
     return ConversationHandler.END
 
 
@@ -53,3 +63,20 @@ def get_request_amount(bot, update):
 def fetch_left_credit(user):
     # TODO: Implement it
     return 500
+
+
+# Places a debt request
+def place_debt(user_id, amount):
+    result = db.insert_debt(user_id, amount)
+    return result
+
+
+# Notifies the friends of a user to pay for him
+def notify_friends(bot, user_id, debt_id):
+    debt = db.get_debt(debt_id)
+    friends = db.get_friend_ids(user_id)
+
+    for friend in friends:
+        bot.send_message(chat_id=friend,
+                         text=bm.notify_debt_title_1 + str(debt["amount"]) + bm.notify_debt_title_2,
+                         reply_markup=ReplyKeyboardMarkup(notify_debt_keyboard, one_time_keyboard=True))
