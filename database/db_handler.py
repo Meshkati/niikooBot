c073b0800f11145e26c25f4a6f7295474772c643
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-14 10:25:38 +0330

    Implement get_code in db_handler

--- database/db_handler.py
+++ database/db_handler.py
@@ -10,18 +10,29 @@ db_name = "niikoo"
 
 
 # Creates collections if necessary
-def migrate():
+def get_collection(collection_name):
     db = mongo_client[db_name]
-    users = db["users"]
+    return db[collection_name]
 
 
 # Inserts a generated code to user
 def insert_code(user, code):
-    db = mongo_client[db_name]
-    users = db["users"]
+    users = get_collection("users")
     # creating the item
     item = {"user_id": user.id, "code": code}
     # inserts into collection
     result = users.insert_one(item)
 
     return result.inserted_id
+
+
+# Gets the invite code of a user
+def get_code(user):
+    users = get_collection("users")
+    result = users.find_one({"user_id": user.id})
+    print(result)
+
+    if result is None:
+        return None
+
+    return result["code"]
